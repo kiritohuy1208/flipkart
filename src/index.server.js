db23bbeed742591c0c4e7ef3593179467c96b70e
Tô Huy <kiritohuy1208>
2021-08-20 14:39:02 +0700

    edit port

--- src/index.server.js
+++ src/index.server.js
@@ -39,7 +39,7 @@ app.use("/api", addressRoute);
 app.use("/api", orderRoute);
 app.use("/api", orderAdminRoute);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.get("/", (req, res, next) => {
   res.send("Server web Flipkart start on port :" + PORT);
 });
